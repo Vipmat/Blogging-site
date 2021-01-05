50273d10c7efcc5cefff795098a1ceef4ad1d867
Vipul Daripkar <vipuldaripkar@gmail.com>
2021-01-05 08:43:10 +0000

    Created a simple navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { NavbarComponent } from './navigation/header/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
